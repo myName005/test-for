68c8ff78f3f9c0b4678fe1c0b0b8c08d60757fb3
myName005 <eer37779@gmail.com>
2017-08-10 15:21:43 +0100

    use vue production cdn instead of dev cdn add calculateCOst method add computed property totalcost

--- js/main.js
+++ js/main.js
@@ -1,10 +1,30 @@
 var app = new Vue({
+	el:"#app",
 	data:{
-
+		productsList:[
+		],
+		input:{
+			name:"",
+			quantity:0,
+			cpu:0
+		}
+	},
+	methods:
+	{
+		calculateCost:function (elm) {
+			return elm.cpu * elm.quantity;
+		}
 	},
 	computed:{
 		totalCost: function () {
-			var list = this.
+			var list = this.productsList;
+			var total = 0
+			for(var i =0;i<list.length;i++)
+			{
+				total += list[i].cpu * list[i].quantity;
+			}
+			return total;
 		}
+
 	}
 });
